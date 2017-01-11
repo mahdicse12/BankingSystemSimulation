374b4e2a77c1744a4028c36a12d621ed8e198d05
mahdicse12 <mahdicse12@gmail.com>
2017-01-11 12:47:05 +0600

    C#

---
+++ BankingSystemSimulation/BankingSystemSimulation.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace BankingSystemSimulation
+{
+    partial class BankingSystemSimulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_SFS = new System.Windows.Forms.Label();
+            this.label_SSS = new System.Windows.Forms.Label();
+            this.textBox_MST3 = new System.Windows.Forms.TextBox();
+            this.label_MST3 = new System.Windows.Forms.Label();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Evaluate = new System.Windows.Forms.Button();
+            this.textBox_NOC = new System.Windows.Forms.TextBox();
+            this.label_NOC = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label_STS = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_MST1 = new System.Windows.Forms.TextBox();
+            this.label_MST1 = new System.Windows.Forms.Label();
+            this.label_MIT = new System.Windows.Forms.Label();
+            this.textBox_MIT = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_MST2 = new System.Windows.Forms.TextBox();
+            this.label_MST2 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_SFS
+            // 
+            this.label_SFS.AutoSize = true;
+            this.label_SFS.Location = new System.Drawing.Point(7, 16);
+            this.label_SFS.Name = "label_SFS";
+            this.label_SFS.Size = new System.Drawing.Size(0, 13);
+            this.label_SFS.TabIndex = 1;
+            // 
+            // label_SSS
+            // 
+            this.label_SSS.AutoSize = true;
+            this.label_SSS.Location = new System.Drawing.Point(7, 16);
+            this.label_SSS.Name = "label_SSS";
+            this.label_SSS.Size = new System.Drawing.Size(0, 13);
+            this.label_SSS.TabIndex = 0;
+            // 
+            // textBox_MST3
+            // 
+            this.textBox_MST3.Location = new System.Drawing.Point(274, 130);
+            this.textBox_MST3.Name = "textBox_MST3";
+            this.textBox_MST3.Size = new System.Drawing.Size(236, 20);
+            this.textBox_MST3.TabIndex = 4;
+            // 
+            // label_MST3
+            // 
+            this.label_MST3.Location = new System.Drawing.Point(110, 130);
+            this.label_MST3.Name = "label_MST3";
+            this.label_MST3.Size = new System.Drawing.Size(158, 25);
+            this.label_MST3.TabIndex = 32;
+            this.label_MST3.Text = "Mean Service Time (Server 3) :";
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(423, 194);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(87, 27);
+            this.button_Clear.TabIndex = 7;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Evaluate
+            // 
+            this.button_Evaluate.Location = new System.Drawing.Point(316, 194);
+            this.button_Evaluate.Name = "button_Evaluate";
+            this.button_Evaluate.Size = new System.Drawing.Size(87, 27);
+            this.button_Evaluate.TabIndex = 6;
+            this.button_Evaluate.Text = "Evaluate";
+            this.button_Evaluate.UseVisualStyleBackColor = true;
+            this.button_Evaluate.Click += new System.EventHandler(this.button_Evaluate_Click);
+            // 
+            // textBox_NOC
+            // 
+            this.textBox_NOC.Location = new System.Drawing.Point(274, 168);
+            this.textBox_NOC.Name = "textBox_NOC";
+            this.textBox_NOC.Size = new System.Drawing.Size(236, 20);
+            this.textBox_NOC.TabIndex = 5;
+            // 
+            // label_NOC
+            // 
+            this.label_NOC.Location = new System.Drawing.Point(110, 168);
+            this.label_NOC.Name = "label_NOC";
+            this.label_NOC.Size = new System.Drawing.Size(158, 25);
+            this.label_NOC.TabIndex = 28;
+            this.label_NOC.Text = "Number Of Customers :";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label_SSS);
+            this.groupBox3.Location = new System.Drawing.Point(306, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(297, 172);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Report Second Server";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox6);
+            this.groupBox4.Controls.Add(this.groupBox3);
+            this.groupBox4.Controls.Add(this.groupBox2);
+            this.groupBox4.Location = new System.Drawing.Point(15, 251);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(609, 348);
+            this.groupBox4.TabIndex = 29;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Output";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.label_STS);
+            this.groupBox6.Location = new System.Drawing.Point(6, 199);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(294, 143);
+            this.groupBox6.TabIndex = 9;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Report Third Server";
+            // 
+            // label_STS
+            // 
+            this.label_STS.AutoSize = true;
+            this.label_STS.Location = new System.Drawing.Point(7, 16);
+            this.label_STS.Name = "label_STS";
+            this.label_STS.Size = new System.Drawing.Size(0, 13);
+            this.label_STS.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_SFS);
+            this.groupBox2.Location = new System.Drawing.Point(6, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 172);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Report First Server";
+            // 
+            // textBox_MST1
+            // 
+            this.textBox_MST1.Location = new System.Drawing.Point(274, 55);
+            this.textBox_MST1.Name = "textBox_MST1";
+            this.textBox_MST1.Size = new System.Drawing.Size(236, 20);
+            this.textBox_MST1.TabIndex = 2;
+            // 
+            // label_MST1
+            // 
+            this.label_MST1.Location = new System.Drawing.Point(110, 55);
+            this.label_MST1.Name = "label_MST1";
+            this.label_MST1.Size = new System.Drawing.Size(158, 25);
+            this.label_MST1.TabIndex = 26;
+            this.label_MST1.Text = "Mean Service Time (Server 1) :";
+            // 
+            // label_MIT
+            // 
+            this.label_MIT.Location = new System.Drawing.Point(110, 16);
+            this.label_MIT.Name = "label_MIT";
+            this.label_MIT.Size = new System.Drawing.Size(158, 25);
+            this.label_MIT.TabIndex = 24;
+            this.label_MIT.Text = "Mean Interarrival Time :";
+            // 
+            // textBox_MIT
+            // 
+            this.textBox_MIT.Location = new System.Drawing.Point(274, 16);
+            this.textBox_MIT.Name = "textBox_MIT";
+            this.textBox_MIT.Size = new System.Drawing.Size(236, 20);
+            this.textBox_MIT.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_MST2);
+            this.groupBox1.Controls.Add(this.label_MST2);
+            this.groupBox1.Controls.Add(this.textBox_MST3);
+            this.groupBox1.Controls.Add(this.label_MST3);
+            this.groupBox1.Controls.Add(this.button_Clear);
+            this.groupBox1.Controls.Add(this.button_Evaluate);
+            this.groupBox1.Controls.Add(this.textBox_NOC);
+            this.groupBox1.Controls.Add(this.label_NOC);
+            this.groupBox1.Controls.Add(this.textBox_MST1);
+            this.groupBox1.Controls.Add(this.label_MST1);
+            this.groupBox1.Controls.Add(this.textBox_MIT);
+            this.groupBox1.Controls.Add(this.label_MIT);
+            this.groupBox1.Location = new System.Drawing.Point(15, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(609, 232);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // textBox_MST2
+            // 
+            this.textBox_MST2.Location = new System.Drawing.Point(274, 92);
+            this.textBox_MST2.Name = "textBox_MST2";
+            this.textBox_MST2.Size = new System.Drawing.Size(236, 20);
+            this.textBox_MST2.TabIndex = 3;
+            // 
+            // label_MST2
+            // 
+            this.label_MST2.Location = new System.Drawing.Point(110, 92);
+            this.label_MST2.Name = "label_MST2";
+            this.label_MST2.Size = new System.Drawing.Size(158, 25);
+            this.label_MST2.TabIndex = 35;
+            this.label_MST2.Text = "Mean Service Time (Server 2) :";
+            // 
+            // BankingSystemSimulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BankingSystemSimulation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Banking System Simulation";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_SFS;
+        private System.Windows.Forms.Label label_SSS;
+        private System.Windows.Forms.TextBox textBox_MST3;
+        private System.Windows.Forms.Label label_MST3;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Evaluate;
+        private System.Windows.Forms.TextBox textBox_NOC;
+        private System.Windows.Forms.Label label_NOC;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label_STS;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_MST1;
+        private System.Windows.Forms.Label label_MST1;
+        private System.Windows.Forms.Label label_MIT;
+        private System.Windows.Forms.TextBox textBox_MIT;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_MST2;
+        private System.Windows.Forms.Label label_MST2;
+    }
+}
+
---
+++ BankingSystemSimulation/BankingSystemSimulation.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankingSystemSimulation
+{
+    public partial class BankingSystemSimulation : Form
+    {
+        public BankingSystemSimulation()
+        {
+            InitializeComponent();
+        }
+        
+        private void button_Evaluate_Click(object sender, EventArgs e)
+        {
+            if (textBox_MIT.Text == "" || textBox_MST1.Text == "" || textBox_MST2.Text == "" || textBox_MST3.Text == "" || textBox_NOC.Text == "")
+            {
+                return;
+            }
+
+            Server s = new Server();
+
+            s.mean_interarrival = Convert.ToDouble(textBox_MIT.Text);
+            s.mean_service[1] = Convert.ToDouble(textBox_MST1.Text);
+            s.mean_service[2] = Convert.ToDouble(textBox_MST2.Text);
+            s.mean_service[3] = Convert.ToDouble(textBox_MST3.Text);
+            s.num_delays_required = Convert.ToInt32(textBox_NOC.Text);
+
+            s.initialize();
+
+            Random ran = new Random();
+            int count = 0;
+            while (s.num_custs_delayed[1] < s.num_delays_required)
+            {
+                s.timing();
+
+                switch (s.next_event_type)
+                {
+                    case 1:
+                        s.arrive(s.sim_time , 1);
+                        s.update_time_avg_stats(1);
+                        break;
+                    case 2:
+                        s.depart(1);
+                        s.update_time_avg_stats(1);
+                        
+                        if (ran.NextDouble() > .45)
+                        {
+                            s.arrive(s.sim_time, 2);
+                            s.update_time_avg_stats(2);
+                            count++;
+                        }
+                        break;
+                    
+                    case 3:
+
+                        s.depart(2);
+                        s.update_time_avg_stats(2);
+
+                        if (ran.NextDouble() > .4)
+                        {
+                            s.arrive(s.sim_time, 3);
+                            s.update_time_avg_stats(3);
+                        }
+                        break;
+                    
+                    case 4:
+                        s.depart(3);
+                        s.update_time_avg_stats(3);
+                        break;
+                }
+                
+            }
+
+            s.time_next_event[1] = Double.MaxValue;
+            s.num_in_q[1] = 0;
+
+            //Console.WriteLine(count);
+
+            while (s.num_custs_delayed[2] < count)
+            {
+                s.timing();
+
+                switch (s.next_event_type)
+                {
+                    case 1:
+                        s.arrive(s.sim_time, 1);
+                        s.update_time_avg_stats(1);
+                        break;
+                    case 2:
+                        s.depart(1);
+                        s.update_time_avg_stats(1);
+
+                        if (ran.NextDouble() > .45)
+                        {
+                            s.arrive(s.sim_time, 2);
+                            s.update_time_avg_stats(2);
+                        }
+                        break;
+
+                    case 3:
+
+                        s.depart(2);
+                        s.update_time_avg_stats(2);
+
+                        if (ran.NextDouble() > .4)
+                        {
+                            s.arrive(s.sim_time, 3);
+                            s.update_time_avg_stats(3);
+                        }
+                        break;
+
+                    case 4:
+                        s.depart(3);
+                        s.update_time_avg_stats(3);
+                        break;
+                }
+
+            }
+
+
+            while (s.num_in_q[3] > 0)
+            {
+                s.timing();
+                s.depart(3);
+                s.update_time_avg_stats(3);
+            }
+
+            label_SFS.Text = s.report(1);
+            label_SSS.Text = s.report(2);
+            label_STS.Text = s.report(3);
+            
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox_MIT.ResetText();
+            textBox_MST1.ResetText();
+            textBox_MST2.ResetText();
+            textBox_MST3.ResetText();
+            textBox_NOC.ResetText();
+            label_SFS.ResetText();
+            label_SSS.ResetText();
+            label_STS.ResetText();
+        }
+
+    class Server
+    {
+            const int Q_LIMIT = 100;
+            const int BUSY = 1;
+            const int IDLE = 0;
+
+            public int next_event_type;
+            public int []num_custs_delayed = new int[4];
+            public int num_delays_required;
+            int num_events = 4;
+            public int []num_in_q = new int[4];
+            int []server_status = new int[4];
+
+            double []area_num_in_q = new double[4];
+            double []area_server_status = new double[4];
+            public double mean_interarrival;
+            public double[] mean_service = new double[4];
+            public double sim_time;
+            double[,] time_arrival = new double[4, Q_LIMIT + 1];
+            double []time_last_event = new double[4];
+            public double[] time_next_event = new double[5];
+            double []total_of_delays = new double[4];
+
+            string result;
+            
+            Random random = new Random();
+            public void initialize()
+            {
+                sim_time = 0.0;
+
+                server_status[1] = IDLE;
+                server_status[2] = IDLE;
+                server_status[3] = IDLE;
+
+                num_in_q[1] = 0;
+                num_in_q[2] = 0;
+                num_in_q[3] = 0;
+
+                time_last_event[1] = 0.0;
+                time_last_event[2] = 0.0;
+                time_last_event[3] = 0.0;
+
+                num_custs_delayed[1] = 0;
+                num_custs_delayed[2] = 0;
+                num_custs_delayed[3] = 0;
+
+                total_of_delays[1] = 0.0;
+                total_of_delays[2] = 0.0;
+                total_of_delays[3] = 0.0;
+
+                area_num_in_q[1] = 0.0;
+                area_num_in_q[2] = 0.0;
+                area_num_in_q[3] = 0.0;
+
+                area_server_status[1] = 0.0;
+                area_server_status[2] = 0.0;
+                area_server_status[3] = 0.0;
+
+                time_next_event[1] = sim_time + expon(mean_interarrival);
+                time_next_event[2] = Double.MaxValue ;
+                time_next_event[3] = Double.MaxValue;
+                time_next_event[4] = Double.MaxValue;
+
+            }
+
+            public void timing()
+            {
+                double min_time_next_event = Double.MaxValue;
+                next_event_type = 0;
+
+                for (int i = 1; i <= num_events; ++i)
+                {
+                    if (time_next_event[i] < min_time_next_event)
+                    {
+                        min_time_next_event = time_next_event[i];
+                        next_event_type = i;
+                    }
+                }
+
+                if (next_event_type == 0)
+                {
+                    MessageBox.Show("Event list empty at time "+sim_time);
+                    Application.Exit();
+                }
+                sim_time = min_time_next_event;
+            }
+
+            public void arrive(double newTime, int ServerName)
+            {
+                double delay;
+                if (ServerName == 1)
+                {
+                    time_next_event[1] = sim_time + expon(mean_interarrival);
+                }
+                
+                if (server_status[ServerName] == BUSY)
+                {
+                    ++num_in_q[ServerName] ;
+                    
+                    if (num_in_q[ServerName] > Q_LIMIT)
+                    {
+                        MessageBox.Show("Over flow of the queue time arrival at time " + sim_time);
+                        Application.Exit();
+                    }
+
+                    time_arrival[ServerName ,num_in_q[ServerName]] = newTime;
+                }
+
+                else
+                {
+                    delay = 0.0;
+                    total_of_delays[ServerName] += delay;
+
+                    ++num_custs_delayed[ServerName];
+                    server_status[ServerName] = BUSY;
+
+                    time_next_event[ServerName+1] = sim_time + expon(mean_service[ServerName]);
+
+                }
+            }
+
+            public void depart(int ServerName)
+            {
+                double delay;
+
+                if (num_in_q[ServerName] == 0)
+                {
+                    server_status[ServerName] = IDLE;
+                    time_next_event[ServerName + 1] = Double.MaxValue;
+                }
+                else
+                {
+                    --num_in_q[ServerName];
+
+                    delay = sim_time - time_arrival[ServerName ,1];
+                    total_of_delays[ServerName] += delay;
+
+                    ++num_custs_delayed[ServerName];
+
+                    time_next_event[ServerName + 1] = sim_time + expon(mean_service[ServerName]);
+
+                    for (int i = 1; i <= num_in_q[ServerName]; ++i)
+                    {
+                        if (i == Q_LIMIT) break;
+                        time_arrival[ServerName, i] = time_arrival[ServerName, (i + 1)];
+                    }
+                }
+            }
+
+            public string report(int ServerName)
+            {
+                result = "Average delay in queue " + (total_of_delays[ServerName] / num_custs_delayed[ServerName]) + " minutes\n\n"
+                                + "Average number in queue " + (area_num_in_q[ServerName] / sim_time) + "\n\n"
+                                + "Server utilization " + (area_server_status[ServerName] / sim_time) + "\n\n"
+                                + "Time simulation ended " + sim_time + " minutes\n\n"
+                                + "Total customer who got service " + num_custs_delayed[ServerName] + "\n\n";
+                
+                return result;
+            }
+
+            public void update_time_avg_stats(int ServerName)
+            {
+                double time_since_last_event;
+
+                time_since_last_event = sim_time - time_last_event[ServerName];
+                time_last_event[ServerName] = sim_time;
+
+                area_num_in_q[ServerName] += num_in_q[ServerName] * time_since_last_event;
+
+                area_server_status[ServerName] += server_status[ServerName] * time_since_last_event;
+            }
+            public double expon(double mean)
+            {
+                return (-mean * Math.Log(random.NextDouble()));
+            }
+        }
+    }
+}
